de2ed6ed0dd9dfc3c426d83c1a33b47cf9256954
Jeff Piquette <Jeff.Piquette@metaltek.com>
2017-03-17 16:55:15 -0500

    made a nice commandline interface

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
+import os
+import sys
 from app import app
 
 try:
     import config
+    if __name__ == '__main__':
+        if config.prod["DEBUG"] == None or config.prod["DEBUG"] == True:
+            os.environ["FLASK_DEBUG"] = "True"
+        os.environ["FLASK_APP"] = sys.argv[0]
+        os.system(sys.executable + " -m flask " + " ".join(sys.argv[1:]) )
 except ImportError:
     print("No config.py found!")
     exit(1)
